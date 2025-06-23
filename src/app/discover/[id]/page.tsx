576dcb92174fd950dbc748259fe36cc34a129382
Shunsuke0205 <hirata-shunsuke1125@g.ecc.u-tokyo.ac.jp>
2025-06-23 23:02:56 +0900

    impl: /discover/[id]/page.tsx  Get the id.

---
+++ src/app/discover/[id]/page.tsx
@@ -0,0 +1,16 @@
+
+
+
+export default async function Page({
+  params,
+} : {
+  params: Promise<{ id : string }>
+}) {
+  const { id } = await params;
+
+  return (
+    <div>
+      Activity ID: {id}
+    </div>
+  );
+}
