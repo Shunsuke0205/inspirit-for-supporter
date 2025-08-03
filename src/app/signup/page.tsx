fc5fe8bc71169e5c86b563a0e9b11832ce48f0d0
Shunsuke0205 <hirata-shunsuke1125@g.ecc.u-tokyo.ac.jp>
2025-08-03 17:35:30 +0900

    impl: /signup/page.tsx

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,14 @@
+import { signup } from '../login/actions'
+
+export default function SignupPage() {
+  return (
+    <form>
+      <label htmlFor="email">Email:</label>
+      <input id="email" name="email" type="email" required />
+      <label htmlFor="password">Password:</label>
+      <input id="password" name="password" type="password" required />
+      <button formAction={signup}>Sign up</button>
+    </form>
+  )
+}
+
