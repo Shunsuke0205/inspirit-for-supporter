4eb93d1ae325802dd0bc723de67a194493ff0206
Shunsuke0205 <hirata-shunsuke1125@g.ecc.u-tokyo.ac.jp>
2025-08-03 20:24:53 +0900

    modify the app name

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -21,7 +21,7 @@ const Header = async () => {
           height={50}
           className=""
         />
-        <h1 className="ml-4 text-2xl">えん</h1>
+        <h1 className="ml-4 text-2xl">高校生のクラウドファンディング</h1>
       </Link>
       {isLoggedIn ?
         <div>
