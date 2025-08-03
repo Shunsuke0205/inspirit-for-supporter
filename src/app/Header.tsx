30e1cb49d3c8d2f2ad0a1a034668d7bd39f24cac
Shunsuke0205 <hirata-shunsuke1125@g.ecc.u-tokyo.ac.jp>
2025-08-03 15:34:00 +0900

    impl: Header component

---
+++ src/app/Header.tsx
@@ -0,0 +1,47 @@
+import { createClient } from "@/utils/supabase/server";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const Header = async () => {
+  const supabase = await createClient();
+  const { data: userData, error: userError } = await supabase.auth.getUser();
+  const isLoggedIn = !userError && (userData?.user !== null);
+
+  return (
+    <header className="py-4 border-b border-gray-300 flex justify-between items-center ">
+      <Link
+        href="/"
+        className="flex items-center"
+      >
+        <Image
+          src="/product_icon.png"
+          alt="Icon"
+          width={50}
+          height={50}
+          className=""
+        />
+        <h1 className="ml-4 text-2xl">えん</h1>
+      </Link>
+      {isLoggedIn ?
+        <div>
+          <Link
+            href={`/profile/${userData.user.id}`}
+            className="border border-gray-300 px-3 py-1 bg-orange-100 cursor-pointer rounded-lg hover:bg-orange-100"
+          >
+            マイページ
+          </Link>
+        </div>
+        :
+        <Link
+          href="/login"
+          className="border border-gray-300 px-3 py-1 bg-orange-100 cursor-pointer rounded-lg hover:bg-orange-200"
+        >
+          ログイン
+        </Link>
+      }
+    </header>
+  );
+};
+
+export default Header;
