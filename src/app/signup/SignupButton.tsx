fac51df51ea62f4162b6c3150585e148860c31a5
Shunsuke0205 <hirata-shunsuke1125@g.ecc.u-tokyo.ac.jp>
2026-01-06 22:20:43 +0900

    modify sentense

--- src/app/signup/SignupButton.tsx
+++ src/app/signup/SignupButton.tsx
@@ -14,7 +14,7 @@ export const SignupButton = () => {
           w-full py-3 px-4 bg-indigo-600 text-white font-semibold rounded-lg shadow-md hover:bg-indigo-700 transition duration-300 ease-in-out
           disabled:cursor-not-allowed disabled:bg-indigo-300"
       >
-        {pending ? "お待ちになってください..." : "新規登録"}
+        {pending ? "少しお待ちになってください..." : "新規登録"}
       </button>
     </div>
   );
