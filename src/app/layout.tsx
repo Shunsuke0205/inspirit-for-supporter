ca0bcf001aed9bca4532dd6228b2bd1424934252
Shunsuke0205 <hirata-shunsuke1125@g.ecc.u-tokyo.ac.jp>
2026-06-28 14:04:59 +0900

    add: Footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Header from "./Header";
+import Footer from "./Footer";
 
 
 
@@ -21,6 +22,7 @@ export default function RootLayout({
         <main>
           {children}
         </main>
+        <Footer />
       </body>
     </html>
   );
